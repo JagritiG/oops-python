4ce1637eddac5f8575f27198bc1f5c8a641eedc6
santanusarma <jagritigoswami84@gmail.com>
2019-02-23 11:24:26 -0800

    Add real-estate

---
+++ real-estate/real_estate.py
@@ -0,0 +1,4 @@
+# Book: Python 3 Object Oriented Programming
+# Chapter 3: Case study
+# A simple real estate application that allows an agent to manage properties available 
+# for purchase or rent.
